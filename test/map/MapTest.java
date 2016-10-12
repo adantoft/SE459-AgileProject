eae26bb71a30286bd25280ee372bd28d64a64d5d
cpmarci <philip.marcille@gmail.com>
2016-10-12 15:39:38 -0500

    Frame Work to ensure Intellij setup correctly

---
+++ test/map/MapTest.java
@@ -0,0 +1,31 @@
+package map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by phili_000 on 10/12/2016.
+ */
+public class MapTest {
+    @Test
+    public void printMap() throws Exception {
+
+    }
+
+    @Test
+    public void setFloor() throws Exception {
+
+    }
+
+    @Test
+    public void attachTiles() throws Exception {
+
+    }
+
+    @Test
+    public void setSpace() throws Exception {
+
+    }
+
+}
