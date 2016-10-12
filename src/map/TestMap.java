a255e9bae57a903c14cd6f81ad18fd2e2dff32df
Adnan Dossaji <nan.dossaji@gmail.com>
2016-10-11 22:54:05 -0500

    Mapped the Example in our Project Manual

--- src/map/TestMap.java
+++ src/map/TestMap.java
@@ -1,32 +1,67 @@
 package map;
 
 import vacuum.CleanSweep;
-import floor.Tile;
+import map.Point;
+import map.Map;
+import map.Space;
 import floor.Tile.Floor;
-import general.DataValidationException;
 
 public class TestMap {
 	
 	public static void main(String[] args) {
 		CleanSweep cs = CleanSweep.getInstance();
+
+		Map map = new Map(10, 10);
+
+		Space guestBedroomA = new Space(new Point(0, 0), new Point(3, 4));
+		map.setSpace(guestBedroomA, Floor.LOW);
+
+		Space guestBedroomACloset = new Space(new Point(0, 5), new Point(3, 5));
+		map.setSpace(guestBedroomACloset, Floor.BARE);
+
+		Space guestBedroomB = new Space(new Point(0, 7), new Point(3, 9));
+		map.setSpace(guestBedroomB, Floor.LOW);
+
+		Space guestBedroomBCloset = new Space(new Point(0, 6), new Point(1, 6));
+		map.setSpace(guestBedroomBCloset, Floor.BARE);
+
+		Space hallway = new Space(new Point(4, 0), new Point(5, 7));
+		map.setSpace(hallway, Floor.BARE);
+
+		Space hallwayCloset = new Space(new Point(2, 6), new Point(3, 6));
+		map.setSpace(hallwayCloset, Floor.BARE);
+
+		Space hallwayBathroom = new Space(new Point(4, 8), new Point(5, 8));
+		map.setSpace(hallwayBathroom, Floor.BARE);
+
+		Space masterBedroomA = new Space(new Point(6, 0), new Point(9, 6));
+		map.setSpace(masterBedroomA, Floor.BARE);
+		Space masterBedroomB = new Space(new Point(6, 7), new Point(7, 8));
+		map.setSpace(masterBedroomB, Floor.BARE);
+		Space combineMasterAB = new Space(new Point(6, 6), new Point(7, 7));
+		map.attachSpace(combineMasterAB);
+		Space masterHighPile = new Space(new Point(7, 1), new Point(8, 5));
+		map.setFloor(masterHighPile, Floor.HIGH);
+
+		Space masterBedroomBathroom = new Space(new Point(4, 9), new Point(7, 9));
+		map.setSpace(masterBedroomBathroom, Floor.BARE);
+
+		Space masterBedroomCloset = new Space(new Point(8, 7), new Point(9, 9));
+		map.setSpace(masterBedroomCloset, Floor.BARE);
+
+		map.printMap();
+	
+		// // char[] path = { 'n', 'n' };
 		
-		Tile t1 = new Tile(2, Floor.BARE);
-		Tile t2 = new Tile(1, Floor.LOW);
-		Tile t3 = new Tile(3, Floor.HIGH);
-		
-		char[] path = { 'n', 'e' };
-		
-		try {
-			t1.attachTile(t2, 'n');
-			t2.attachTile(t3, 'e');
+		// try {
 			
-			cs.setTile(t1);
-			cs.followPath(path);
-			System.out.println(cs.getTile().toString());
+		// 	// cs.setTile(t);
+		// 	// cs.followPath(path);
+		// 	// System.out.println(cs.getTile().toString());
 			
 			
-		} catch (DataValidationException e) {
-			e.printStackTrace();
-		}
+		// } catch (DataValidationException e) {
+		// 	e.printStackTrace();
+		// }
 	}
 }
