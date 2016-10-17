84903b91f4d9552331704a33a34f44d98f8101d8
cpmarci <philip.marcille@gmail.com>
2016-10-16 22:55:07 -0500

    All put 2 methods tested.

--- test/map/MapTest.java
+++ test/map/MapTest.java
@@ -4,6 +4,8 @@ import floor.Tile;
 import org.junit.Test;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -28,34 +30,85 @@ public class MapTest {
     @Test(timeout = 10000)
     public void printMapTest() throws Exception {
 
-        testMap.printMap();
+        map.Map testMapNew = new map.Map(2, 2);
+        testMapNew.printMap();
 
     }
 
 
     @Test(timeout = 10000)
-    public void setFloor() throws Exception {
+    public void setFloorTest() throws Exception {
 
-        testMap.setFloor(testRoom1HighCarp, Tile.Floor.HIGH);
-        testMap.setFloor(testRoom2LowCarp, Tile.Floor.LOW);
-        testMap.setFloor(testRoom3Bare, Tile.Floor.BARE);
+        map.Map testMapNew = new map.Map(2, 2);
+        map.Map testMapNewCompare = new map.Map(2, 2);
+        Space testRoom1HighCarpNew = new Space(new Point(0, 0), new Point(1, 1));
+        testMapNew.setFloor(testRoom1HighCarpNew, Tile.Floor.HIGH);
+        testMapNew.printMap();
+        assertNotNull(testMapNew);
+        assertNotEquals(testMapNewCompare, testMapNew.toString());
 
     }
 
+
+    @Test(timeout = 10000)
+    public void attachTilesTest() throws Exception {
+
+    }
 
 
     @Test
-    public void attachTiles() throws Exception {
+    public void setSpaceTest() throws Exception {
+
+        map.Map testMapNew = new map.Map(2, 2);
+        map.Map testMapNewCompare = new map.Map(2, 2);
+        Space testRoom1HighCarpNew = new Space(new Point(0, 0), new Point(1, 1));
+
+        testMapNew.setSpace(testRoom1HighCarpNew, Tile.Floor.BARE);
+        assertNotEquals(testMapNew.getTile(0,0).toString(), testMapNewCompare.getTile(0,0).toString());
+
+        testMapNewCompare.setSpace(testRoom1HighCarpNew, Tile.Floor.HIGH);
+        assertNotEquals(testMapNew.getTile(0,0).toString(), testMapNewCompare.getTile(0,0).toString());
+
+        testMapNewCompare.setSpace(testRoom1HighCarpNew, Tile.Floor.BARE);
+        assertEquals(testMapNew.getTile(0,0).toString(), testMapNewCompare.getTile(0,0).toString());
 
     }
 
+    @Test(timeout = 10000)
+    public void getTilesTest() throws Exception {
+
+
+    /*
+            public List<Tile> getTiles(){
+        List<Tile> lst = new ArrayList();
+
+        for (int i = 0; i < this.map.length; i ++) {
+            for (int j = 0; j < this.map[0].length; j ++) {
+                lst.add(map[i][j]);
+
+            }
+        }
+        return lst;
+
+    }
+         */
+
+
+
+
+    }
 
     @Test
-    public void setSpace() throws Exception {
+    public void getTileTest() throws Exception {
+
+        map.Map testMapNew = new map.Map(2, 2);
+        Space testRoom1HighCarpNew = new Space(new Point(0, 0), new Point(1, 1));
+        testMapNew.setFloor(testRoom1HighCarpNew, Tile.Floor.HIGH);
+
+
+        assertNotNull(testMapNew.getTile(0,0));
+        assertEquals("Dirt: 0\n" + "Floor type: High pile", testMapNew.getTile(0,0).toString());
+        assertNotEquals("Dirt: 1\n" + "Floor type: High pile", testMapNew.getTile(0,0).toString());
 
     }
-
-
 }
-
-
